c7e469df2b17e92a9f07422be89eb33a316c9893
James Zhou <jam9es@gmail.com>
2024-05-14 10:19:04 -0700

    Add new test for capitalize function

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,7 @@ import { capitalize } from "./capitalize";
 test('capitalize a string', () => {
     expect(capitalize('joe')).toBe('Joe');
 });
+
+test('capitalize a string', () => {
+    expect(capitalize('Jose')).toBe('Jose');
+});
